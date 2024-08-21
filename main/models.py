709b192894d2431f6daeaf51b5680980022c58cc
pranit111 <69381536+pranit111@users.noreply.github.com>
2024-08-21 23:35:42 +0530

    all features dded

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 class Property(models.Model):
     PROPERTY_TYPES = [
         ('house', 'House'),
@@ -13,7 +14,7 @@ class Property(models.Model):
         ('readytomove', 'Ready To Move'),
         ('underconstrution', 'Under Construction'),
     ]
-    main_img=models.ImageField(upload_to="media/images",default="")
+    main_img = models.ImageField(upload_to="media/images", default="")
     title = models.CharField(max_length=200)
     description = models.TextField()
     address = models.CharField(max_length=255)
@@ -21,19 +22,36 @@ class Property(models.Model):
     state = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=20)
     property_type = models.CharField(max_length=50, choices=PROPERTY_TYPES)
-    property_status = models.CharField(max_length=50, choices=PROPERTY_STATUS,null=True)
+    property_status = models.CharField(max_length=50, choices=PROPERTY_STATUS, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     bedrooms = models.IntegerField()
     bathrooms = models.IntegerField()
     square_feet = models.IntegerField()
-    map_url=models.TextField(null=True)
+    map_url = models.TextField(null=True)
+    video = models.TextField(null=True)
     lot_size = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
     year_built = models.IntegerField(blank=True, null=True)
     is_published = models.BooleanField(default=True)
-    list_date = models.DateTimeField(auto_now_add=True)    
-    devloper=models.TextField(max_length=300,null=True)
+    list_date = models.DateTimeField(auto_now_add=True)
+    devloper = models.TextField(max_length=300, null=True)
+    price2 = models.CharField(max_length=255, editable=False,default='NONE')  # Field for storing the price in words
+
+    def convert_to_lakh_or_crore(self, value):
+        if value >= 1_00_00_000:
+            return f"{value / 1_00_00_000:.2f} crore"
+        elif value >= 1_00_000:
+            return f"{value / 1_00_000:.2f} lakh"
+        else:
+            return str(value)
+
+    def save(self, *args, **kwargs):
+        # Convert the price to lakhs or crores and store it in price2
+        self.price2 = self.convert_to_lakh_or_crore(float(self.price))
+        super(Property, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.title
+
 
 class Photo(models.Model):
     property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='photos')
--- ncpr/urls.py
+++ ncpr/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
     
 
 ]
