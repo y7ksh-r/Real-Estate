f667228d9b1ee3e4b6b7209cabc1ce17dadbe736
pranit111 <69381536+pranit111@users.noreply.github.com>
2024-08-31 18:35:28 +0530

    upto otp

--- main/urls.py
+++ main/urls.py
@@ -7,9 +7,12 @@ urlpatterns = [
     path('Propertyfilter/',views.filter_obj,name='filteroption'),
     path('Propertyfilter/ajax/load-projects/', views.load_projects, name='ajax_load_projects'),
     path('prop_view/<int:pid>', views.prop_view, name='prop_view'),
-    path('contactus/', views.abt_us, name='contact_us'),
+    path('contactus', views.abt_us, name='contact_us'),
     path('moreimages/<int:pid>', views.more_img, name='more_img'),
     path("__reload__/", include("django_browser_reload.urls")),
-   
-    
+    path('contact', views.contact, name='contact_us'),
+    path('about', views.about, name='contact_us'),
+    path('services', views.services, name='contact_us'),
+    path('special_filter/<int:fid>', views.special_page_filter, name='special_page_filter'),
+    path('verify-otp/', views.verify_otp, name='verify_otp'),
     ]    
--- main/views.py
+++ main/views.py
@@ -5,12 +5,27 @@ from django.db.models import F, ExpressionWrapper, FloatField
 from django.db.models.functions import Abs
 from django.contrib import messages
 # Create your views here.
+from firebase_admin import auth
+
+def verify_otp(request):
+    id_token = request.POST.get('id_token')
+
+    try:
+        decoded_token = auth.verify_id_token(id_token)
+        uid = decoded_token['uid']
+        # Create session or perform other actions
+        return JsonResponse({"status": "success", "uid": uid})
+    except Exception as e:
+        return JsonResponse({"status": "error", "message": str(e)})
+
 def home(request):
     list_proj=Property.objects.all()[:8]
     rtm=Property.objects.filter(property_status="underconstrution")[:4]
     uc=Property.objects.filter(property_status="readytomove")[:4]
-    cities=Property.objects.values_list('city',flat=True).distinct()
-    bedrooms=Property.objects.values_list('bedrooms',flat=True).distinct()
+    cities=Property.objects.values_list('city',flat=True).distinct()   
+    bedrooms=Property.objects.values_list('bedrooms',flat=True).distinct().order_by('bedrooms')
+    cities=sorted(set(cities))
+    print(cities)
     properties_with_efficiency = []
     for property in list_proj:
         if property.bedrooms > 0:  # Avoid division by zero
@@ -23,63 +38,32 @@ def home(request):
     # Extract sorted properties
     sorted_properties = [property for property, efficiency in properties_with_efficiency][:4]
     return render(request,'home.html',{'properties':list_proj,'cities':cities,'rtm':rtm,'uc':uc,'ce':sorted_properties,'bedrooms':bedrooms})
+
+def special_page_filter(request,fid):
+    if fid ==1:
+     properties=Property.objects.filter(property_status="underconstrution")
+    if fid ==2:
+     properties=Property.objects.filter(property_status="readytomove")
+    if fid ==3:
+        list_proj=Property.objects.all()
+        properties_with_efficiency = []
+        for property in list_proj:
+         if property.bedrooms > 0:  # Avoid division by zero
+             efficiency = property.price / property.bedrooms
+             properties_with_efficiency.append((property, efficiency))
+    
+    # Sort properties by cost efficiency
+        properties_with_efficiency.sort(key=lambda x: x[1])
+    
+    # Extract sorted properties
+        properties = [property for property, efficiency in properties_with_efficiency]
+    return render(request,'special_filter_page.html',{'properties':properties})
 def load_projects(request):
     city_id = request.GET.get('city_id')
     projects = Property.objects.filter(city=city_id).all()
 
     return JsonResponse(list(projects.values('id', 'title')), safe=False)
-'''def filter_obj(request):
-    city = request.GET.get("city")
-    size = request.GET.get("size")
-    price_range = request.GET.get("price_range")
-    print(city)
-    print(size)
-    print(price_range)
- 
-    price_ranges = {
-        '10': (1000000, 2000000),
-        '20': (2000000, 3000000),
-        '30': (3000000, 5000000),
-        '50': (5000000, 7000000),
-        '70': (7000000, 9999999),
-        '1': (10000000, None)
-    }
-    
-    min_price, max_price = price_ranges.get(price_range, (None, None))
-    property_type_list = ['house', 'apartment', 'villa', 'plot']
-    status_list = ['under_construction', 'ready_to_move']
-    # Start with all properties
-    properties = Property.objects.all()
 
-    # Apply city filter if specified
-    if city:
-        properties = properties.filter(city=city)
-        
-    # Apply project filter if specified
-    if size:
-        properties = properties.filter(bedrooms=size)
-
-    # Apply price range filter if specified
-    if min_price is not None:
-        properties = properties.filter(price__gte=min_price)
-    if max_price is not None:
-        properties = properties.filter(price__lte=max_price)
-        
-
-    # Retrieve distinct cities
-    cities = Property.objects.values_list('city', flat=True).distinct()
-    print(properties)
-    return render(request, 'prop_filtter.html', {
-        'properties': properties,
-        'cities': cities,
-        'selected_city': city,
-        'selected_project': size,
-        'selected_price_range': price_range,
-        'property_type_options': property_type_list,
-
-        'status_options': status_list,
-    }
-    )'''
 
 def prop_view(request,pid):
    property = Property.objects.get(id=pid)
@@ -119,7 +103,7 @@ def abt_us(request):
     inquiry.save()
 
 
-    return redirect("/main")
+    return redirect("/")
 def more_img(request,pid):
     property = Property.objects.get(id=pid)
     photos=Photo.objects.filter(property=pid)
@@ -130,6 +114,7 @@ def filter_obj(request):
     size = request.GET.get("size")
     price_range = request.GET.get("price_range")
     projects_in_city = Property.objects.filter(city=city).values_list('title', flat=True)
+    
     price_ranges = {
         '10': (1000000, 2000000),
         '20': (2000000, 3000000),
@@ -144,25 +129,30 @@ def filter_obj(request):
     # Start with all properties
     properties = Property.objects.all()
 
-    # Apply city filter if specified
-    if city:
+    # Apply city filter if specified and city is not 0
+    if city and city != '0':
         properties = properties.filter(city=city)
 
-    # Apply size filter if specified
-    if size:
+    # Apply size filter if specified and size is not 0
+    if size and size != '0':
         properties = properties.filter(bedrooms=size)
 
-    # Apply price range filter if specified
-    if min_price is not None:
-        properties = properties.filter(price__gte=min_price)
-    if max_price is not None:
-        properties = properties.filter(price__lte=max_price)
+    # Apply price range filter if specified and price range is not 0
+    if price_range and price_range != '0':
+        if min_price is not None:
+            properties = properties.filter(price__gte=min_price)
+        if max_price is not None:
+            properties = properties.filter(price__lte=max_price)
 
     # Retrieve distinct cities and sizes
     cities = Property.objects.values_list('city', flat=True).distinct()
-    bedrooms = Property.objects.values_list('bedrooms', flat=True).distinct()
+    cities=sorted(set(cities))
+    bedrooms = Property.objects.values_list('bedrooms', flat=True).distinct().order_by('bedrooms')
+
+    # Apply additional filters based on projects in city, if applicable
     if projects_in_city:
-        properties ,selected_project_in_city = filter_by_projects_in_city(request,properties,)
+        properties, selected_project_in_city = filter_by_projects_in_city(request, properties)
+
     # Apply additional filters and get the selected values
     properties, selected_property_types, selected_price_order, selected_status = filter_properties(request, properties)
 
@@ -179,10 +169,10 @@ def filter_obj(request):
         'selected_status': selected_status,
         'property_type_options': ['house', 'apartment', 'villa', 'plot'],
         'status_options': ['under construction', 'ready to move'],
-        'projects_in_city':projects_in_city,
-        'selected_project_in_city':selected_project_in_city
-    
+        'projects_in_city': projects_in_city,
+        'selected_project_in_city': selected_project_in_city
     })
+
 
 
 def filter_by_projects_in_city(request,properties):
@@ -218,3 +208,9 @@ def filter_properties(request, properties):
 
     # Return the properties along with the selected filters
     return properties, property_types, price_order, status
+def about(request):
+    return render(request,'about.html')
+def services(request):
+    return render(request,'services.html')
+def contact(request):
+    return render(request,'contact.html')
